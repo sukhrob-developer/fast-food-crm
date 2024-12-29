b4ca9a672eaf7929a02883899bf10c9bd60ee6cd
stohirov <suxrobtoxirov001@gmail.com>
2024-12-29 23:47:46 +0500

    Configurations

---
+++ src/main/java/sukhrob/dev/customer_service/entities/Category.java
@@ -0,0 +1,27 @@
+package sukhrob.dev.customer_service.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import sukhrob.dev.customer_service.entities.template.AbsEntity;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity(name = "categories")
+@SQLDelete(sql = "update categories set deleted=true where id = ?")
+@SQLRestriction(value = "deleted=true")
+public class Category extends AbsEntity {
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private Long parentCategoryId;
+
+}
